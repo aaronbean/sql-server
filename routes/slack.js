e567438de00f89322f23d4f46ab9ed7a0a8321ce
Aaron Bean <aaron.bean@beardon.com>
2014-08-26 15:17:18 -0500

    SQL's gonna chat

--- routes/slack.js
+++ routes/slack.js
@@ -1,10 +1,21 @@
+'use strict';
+
+var config = require('config');
+
 module.exports = function () {
 
     var e = {};
 
     e.postSlack = [
         function (req, res, next) {
-            console.log(req);
+            var message = JSON.parse(req.body);
+            console.log(message);
+            if (message.token == config.app.slack.token) {
+                var response = {
+                    text: '@' + message.user_name + '(squirrel noises)'
+                };
+                return res.json(response);
+            }
             return res.send(200);
         }
     ];
