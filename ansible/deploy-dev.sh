af3650e82732b21f16bad036b079c98991523342
Aaron Bean <aaron.bean@beardon.com>
2015-04-16 13:08:59 -0500

    standardizing ansible

--- ansible/deploy-dev.sh
+++ ansible/deploy-dev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook -i development.ini sql-server.yml -K --vault-password-file ~/.vault_pass.txt
+ansible-playbook -i development.ini site.yml -K --vault-password-file ~/.vault_pass.txt
