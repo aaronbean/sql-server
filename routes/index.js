822d9175b7b1b432070d1d609043d9e400c8b8ab
Aaron Bean <aaron.bean@beardon.com>
2015-05-21 16:25:51 -0500

    return 200 at root

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ module.exports = function (app) {
     var roseRoutes = require('./rose')();
     var slackRoutes = require('./slack')();
 
+    app.get('/', function (req, res, next) {
+        return res.send(200);
+    });
+
     app.route('/rose')
         .get(roseRoutes.getRose);
 
