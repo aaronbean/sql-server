b3d8a5fa6b1333ab6150ba139ca44aa681462e90
Aaron Bean <aaron.bean@beardon.com>
2015-05-22 09:39:21 -0500

    initial IFTTT webhook

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
-app.use(webhook());
+app.use(webhook(require('./lib/ifttt')));
 app.use(bodyParser());
 app.use(methodOverride());
 app.use(cookieParser());
